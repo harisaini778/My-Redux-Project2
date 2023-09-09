77c7df50fe3dc7fc97ec0e350f4c545b7839b790
Hari Kumar Saini <harikumarsaini778@gmail.com>
2023-09-09 10:12:54 +0530

    first commit

---
+++ store/index.js
@@ -0,0 +1,22 @@
+import redux from "redux";
+
+import { legacy_createStore } from "redux";
+
+const counterReducer = (state = { counter: 0 }, action) => {
+    if (action.type === "icrement") {
+        return {
+            counter: state.counter + 1,
+       }
+    }
+    if (action.type === "decrement") {
+        return {
+            counter: state.counter - 1,
+        }
+    }
+
+    return state;
+};
+
+const store = legacy_createStore(counterReducer);
+
+export default store;
