205b38b0ac2f3893901a0c4e8da0099c02785cd3
Hari Kumar Saini <harikumarsaini778@gmail.com>
2023-09-09 13:56:14 +0530

    added toggle functionlity in the project, now user can toggle to show or hide the counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,114 +1,66 @@
-import { Component } from "react";
+import React from "react";
 import { Container, Button } from "react-bootstrap";
 import { Card } from "react-bootstrap";
-// import { useSelector,useDispatch,connect } from "react-redux";
 import { Stack } from "react-bootstrap";
 import { connect } from "react-redux";
 
-// const Counter = () => {
-//     const dispatch = useDispatch();
-
-//     const incrementHandeler = () => {
-//         dispatch({ type: "increment" });
-//     }
-
-//     const decrementHandler = () => {
-//         dispatch({ type: "decrement" });
-//     }
-//     const counter = useSelector(state => state.counter);
-//     const toggleCounterHandeler = () => {
-        
-//     };
-
-//     return (
-//         <div>
-//             <Container className="mt-4">
-//                 <Card>
-//                     <Card.Header>
-//                         <h1>Redux Counter</h1>
-//                     </Card.Header>
-//                     <Card.Body>
-//                         -- <h1>{counter}</h1> --
-//                     </Card.Body>
-//                     <Card.Footer>
-//                         <div className="d-flex justify-content-center">
-//                           <Stack direction="horizontal" gap="3" className="mb-2">
-//                             <Button onClick={incrementHandeler}>
-//                                 Increment
-//                             </Button>
-//                             <Button onClick={decrementHandler}>
-//                                 Decrement 
-//                             </Button>
-//                         </Stack>
-//                         </div>
-//                         <Button onClick={toggleCounterHandeler}>
-//                             Toggle Counter
-//                         </Button>
-//                     </Card.Footer>
-//                 </Card>
-//             </Container>
-//         </div>
-//     );
-// }
-
-class Counter extends Component {
-
-    incrementHandler() { 
-        this.props.increment();
+const Counter = (props) => {
+    const incrementHandler = () => {
+        props.increment();
     };
 
-    decrementHandler() { 
-        this.props.decrement();
+    const decrementHandler = () => {
+        props.decrement();
     };
 
-    increaseHandler() {
-        this.props.increase();
-    }
+    const increaseHandler = () => {
+        props.increase();
+    };
 
-    toggleCounterHandler() { };
+    const toggleCounterHandler = () => {
+        props.toggle();
+    };
 
-    render() {
-        return (<div>
+    return (
+        <div>
             <Container className="mt-4">
                 <Card>
                     <Card.Header>
-                        <h1>Redux Counter</h1>                     </Card.Header>
+                        <h1>Redux Counter</h1>
+                    </Card.Header>
                     <Card.Body>
-                        -- <h1>{this.props.counter}</h1> --
+                        {props.showCounter ? <h1>{props.counter}</h1> : null}
                     </Card.Body>
                     <Card.Footer>
                         <div className="d-flex justify-content-center">
                             <Stack direction="horizontal" gap="3" className="mb-2">
-                                <Button onClick={this.incrementHandler.bind(this)}>
-                                    Increment
-                                </Button>
-                                <Button onClick={this.increaseHandler.bind(this)}>
-                                    Increment By 5
-                                </Button>
-                                <Button onClick={this.decrementHandler.bind(this)}>
-                                    Decrement
-                                </Button>
+                                <Button onClick={incrementHandler}>Increment</Button>
+                                <Button onClick={increaseHandler}>Increment By 5</Button>
+                                <Button onClick={decrementHandler}>Decrement</Button>
                             </Stack>
-                        </div><Button onClick={this.toggleCounterHandler}>
-                            Toggle Counter
-                        </Button>
+                        </div>
+                        <Button onClick={toggleCounterHandler}>Toggle Counter</Button>
                     </Card.Footer>
                 </Card>
             </Container>
-        </div>);
-    }
-}
-const mapStateProps = state => {
-    return {
-    counter : state.counter
-    };
-}
+        </div>
+    );
+};
 
-const mapDispatchProps = dispatch => { 
+const mapStateToProps = (state) => {
+    return {
+        counter: state.counter,
+        showCounter: state.showCounter,
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
     return {
         increment: () => dispatch({ type: "increment" }),
         decrement: () => dispatch({ type: "decrement" }),
-        increase : () => dispatch ({type:"increase",amount:5})
-    }
+        increase: () => dispatch({ type: "increase", amount: 5 }),
+        toggle: () => dispatch({ type: "toggle" }),
+    };
 };
-export default connect(mapStateProps,mapDispatchProps)(Counter);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
